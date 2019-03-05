77c0bf158b524f88bc52e98b21364c1b08b19660
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-03-05 10:26:08 -0800

    fixed username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :likes
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://i.imgur.com/Lb7Uhwk.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates :username, uniqueness: true
+  validates :username, {uniqueness: true, presence: true}
   has_secure_password
 end
