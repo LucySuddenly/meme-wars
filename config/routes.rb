a1ac61b93c6530155ba102dc1fcc47a01990eb03
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-03-06 10:36:46 -0800

    implemented partials for post indices and post submit button

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   get "/dank", to: "posts#dank"
   get "/fresh", to: "posts#fresh"
   get '/top', to: "posts#top"
-  post 'posts/:id', to: "posts#destroy"
   resources :posts do
     resources :likes
     resources :comments do
@@ -19,5 +18,6 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
+  post 'posts/:id', to: "posts#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
